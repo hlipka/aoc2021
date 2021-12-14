4da5f4af32136e97378a6e77fbbf189dc70f3926
Hendrik Lipka <hlipka@salesforce.com>
2021-12-14 11:22:31 +0100

    add day 14

---
+++ src/Day14.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+
+def parse_pattern(line):
+    p = line.split(' -> ')
+    return p[0], p[1]
+
+
+def replace(pattern, patterns):
+    result = [pattern[0]]
+    for pos in range(0, len(pattern)-1):
+        p = pattern[pos]+pattern[pos+1]
+        if p in patterns:
+            c = patterns[p]
+            result.append(c)
+        result.append(pattern[pos+1])
+    return result
+
+
+def run(fname):
+    fin = open(fname)
+    start = fin.readline().strip()
+
+    patterns = {}
+    for line in fin:
+        if line.strip() != "":
+            mapping = parse_pattern(line.strip())
+            patterns[mapping[0]] = mapping[1]
+
+    pattern = list(start)
+    for round in range(0, 10):
+        pattern = replace(pattern, patterns)
+        c = Counter(pattern)
+        print(c)
+
+    print(pattern)
+    print(len(pattern))
+    c = Counter(pattern)
+    print(c)
+    nums = {value: key for key, value in c.items()}
+    print(nums)
+    min_c = min(nums.keys())
+    max_c = max(nums.keys())
+    print(max_c - min_c)
+
+
+if __name__ == '__main__':
+    run('../data/day14.txt')
---
+++ src/Day14b.py
@@ -0,0 +1,62 @@
+def parse_pattern(line):
+    p = line.split(' -> ')
+    return p[0], p[1]
+
+
+def replace(counts, patterns, char_counts):
+    result = dict.fromkeys(counts.keys(), 0)
+    for c in counts:
+        if counts[c] != 0:
+            count = counts[c]
+            repl = patterns[c]
+            r1 = c[0] + repl
+            r2 = repl + c[1]
+            add_char_count(char_counts, repl, count)
+
+            result[r1] = result[r1] + count
+            result[r2] = result[r2] + count
+
+    return result
+
+
+def add_char_count(char_counts, c, count):
+    if c not in char_counts:
+        char_counts[c] = count
+    else:
+        char_counts[c] = char_counts[c] + count
+
+
+def run(fname):
+    fin = open(fname)
+    start = fin.readline().strip()
+
+    patterns = {}
+    counts = {}
+    char_counts = {}
+    for line in fin:
+        if line.strip() != "":
+            mapping = parse_pattern(line.strip())
+            patterns[mapping[0]] = mapping[1]
+            counts[mapping[0]] = 0
+
+    for i in range(0, len(start)-1):
+        p = start[i:i+2]
+        if p in patterns:
+            if p in counts:
+                counts[p] = counts[p] + 1
+            else:
+                counts[p] = 1
+        add_char_count(char_counts, start[i:i+1], 1)
+    add_char_count(char_counts, start[len(start)-1:len(start)], 1)
+
+    for r in range(0, 40):
+        counts = replace(counts, patterns, char_counts)
+
+    min_c = min(char_counts.values())
+    max_c = max(char_counts.values())
+    print(min_c, max_c)
+    print(max_c - min_c)
+
+
+if __name__ == '__main__':
+    run('../data/day14.txt')
