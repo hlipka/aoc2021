de946751f9fa9f63f860cd2913f1429cca2f6add
Hendrik Lipka <hlipka@salesforce.com>
2021-12-22 10:24:33 +0100

    idea for day 22b

--- src/Day22b.py
+++ src/Day22b.py
@@ -15,6 +15,9 @@ class Cube:
             line = line[4:]
             self.value = 0
         match = re.search(pattern, line)
+
+        # TODO sort coordinates so the regions are oriented properly
+
         self.x_start = int(match.group(1))
         self.x_end = int(match.group(2))
         self.y_start = int(match.group(3))
@@ -28,49 +31,55 @@ class Cube:
             self.y_start + y_ofs:self.y_end + y_ofs + 1,
             self.z_start + z_ofs:self.z_end + z_ofs + 1] = self.value
 
+    def overlaps_any(self, regions):
+        for r in regions:
+            if r is not self:
+                if self.overlaps(r):
+                    return True
+        return False
+
+    # FIXME I don't think this check is correct
+    # there is no overlap if for any axis there is no overlap
+    def overlaps(self, region):
+        if self.contains(region.x_start, region.y_start, region.z_start) or \
+           self.contains(region.x_start, region.y_start, region.z_end) or \
+           self.contains(region.x_start, region.y_end, region.z_start) or \
+           self.contains(region.x_start, region.y_end, region.z_end) or \
+           self.contains(region.x_end, region.y_start, region.z_start) or \
+           self.contains(region.x_end, region.y_start, region.z_end) or \
+           self.contains(region.x_end, region.y_end, region.z_start) or \
+           self.contains(region.x_end, region.y_end, region.z_end):
+            return True
+        return False
+
+    def contains(self, x, y, z):
+        return self.x_start <= x < self.x_end and self.y_start <= y < self.y_end and self.z_start <= z < self.z_end
+
 
 def run(f_name):
     fin = open(f_name)
 
     cubes = []
-    x_start = []
-    x_end = []
-    y_start = []
-    y_end = []
-    z_start = []
-    z_end = []
     for line in fin:
         line = line.strip()
         if line == '':
             break
         cube = Cube(line)
         cubes.append(cube)
-        x_start.append(cube.x_start)
-        x_end.append(cube.x_end)
-        y_start.append(cube.y_start)
-        y_end.append(cube.y_end)
-        z_start.append(cube.z_start)
-        z_end.append(cube.z_end)
 
-    x_ofs = - min(x_start)
-    x_max = max(x_end)
-    y_ofs = - min(z_start)
-    y_max = max(z_end)
-    z_ofs = - min(z_start)
-    z_max = max(z_end)
+    # discard any regions which are contained fully in a later region
+    # (they will not do anything)
 
-    amap = np.zeros((x_ofs + x_max + 1, y_ofs + y_max + 1, z_ofs + z_max + 1), dtype=int)
+    # single out all region which do not overlap anything else, and are not contained in an earlier region
+    # keep the 'on' regions, and discard the 'off' regions (they do not do anything)
 
-    for cube in cubes:
-        cube.fill_cube(amap, x_ofs, y_ofs, z_ofs)
+    # go through all regions in order
+    #   for any overlap with an existing region, store a new overlap region with the opposing sign
+    #   if its an 'on' region, store it
 
-    print(amap.shape)
+    # when all steps are finished, run through all regions and count their cubes
 
-    # create a new array which marks all places crossed more than once
-    mark = amap.flat == 1
-    # select these elements, and sum them up
-    crosses = amap.flat[mark]
-    print(sum(crosses))
+    # result for demo data: 2758514936282235
 
 
 if __name__ == '__main__':
